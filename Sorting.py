b45edb942677c4ea5c4eb025a4f18a668c6d5973
gy18 <gy18@ic.ac.uk>
2019-06-23 21:38:16 +0100

    add quick sort (in a simpler manner)

--- Sorting.py
+++ Sorting.py
@@ -68,19 +68,54 @@ def MergetSort(unsorted_list, asceding = True):
     
     return unsorted_list
 
+def QuickSort1(unsorted_array):
+    # lomuto algorithm (pivot is the first argument)
+    unsorted_array = unsorted_array.copy()
+    list_length = len(unsorted_array)
+
+    if (list_length <= 1):
+        return unsorted_array
+    else:
+        pivot = unsorted_array[0]
+        Greater = [element for element in unsorted_array[1:] if element > pivot]
+        Lesser = [element for element in unsorted_array[1:] if element < pivot]
+
+    return QuickSort1(Lesser) + [pivot] + QuickSort1(Greater)
+
+def QuickSort2(unsorted_array):
+    # lomuto algorithm (pivot is the last element)
+
+    unsorted_array = unsorted_array.copy()
+    list_length = len(unsorted_array)
+
+    if (list_length <= 1):
+        return unsorted_array
+    else:
+        pivot = unsorted_array[-1]
+        Greater = [element for element in unsorted_array[:-1] if element > pivot]
+        Lesser = [element for element in unsorted_array[:-1] if element < pivot]
+
+    return QuickSort2(Lesser) + [pivot] + QuickSort2(Greater)
+
+
 if __name__ == "__main__":
 
     array = [5, 1, 4, 2, 8]
     print("Non sorted array")
     print(array)
-
+    
     sorted_array = bubble_sort(array, True)
     print("Now array has been sorted in an ascending order using Bubble Sort")
     print(sorted_array)
-
+    
     sorted_array = bubble_sort(array, False)
     print(sorted_array)
-
+    
     array_ = [64, 25, 12, 22, 11]
     sorted_array_ = selection_sort(array_)
     print(sorted_array_)
+
+    sorted_array_1 = QuickSort1(array)
+    sorted_array_2 = QuickSort2(array)
+    print(sorted_array_1)
+    print(sorted_array_2)
